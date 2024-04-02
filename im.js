468c8acbcbcd079d81c321a4267e520fd82ecf64
Vlad Chugunov <3001420014rd@gmail.com>
2024-04-02 17:44:51 +0300

    ГОЙДАААА

--- im.js
+++ im.js
@@ -1,9 +1,11 @@
 const imageList = ["banner3.png", "banner5.png"];
 const imageList1 = ["smallbann.png", "blacksmallbann.png"];
 const imageList2 = ["plansh.png", "planshblack.png"];
+const imageList3 = ["sun.png", "night.webp"];
 const img = document.querySelector(".img");
 const img2 = document.querySelector(".img2");
 const img3 = document.querySelector(".img3");
+const img4 = document.querySelector(".img4");
 const next = document.getElementById("next");
 var numImage = 0;
 img.src = imageList[numImage];
@@ -12,6 +14,7 @@ document.documentElement.classList.remove("dark", "light");
 document.documentElement.classList.add(themeList[numImage]);
 img2.src = imageList1[numImage];
 img3.src = imageList2[numImage];
+img4.src = imageList3[numImage];
 next.addEventListener("click", () => {
   numImage++;
   if (numImage >= imageList.length) {
@@ -24,6 +27,7 @@ next.addEventListener("click", () => {
   img.src = imageList[numImage];
 
   document.documentElement.classList.add(themeList[numImage]);
+  img4.src = imageList3[numImage];
   img3.src = imageList2[numImage];
   img2.src = imageList1[numImage];
 });
